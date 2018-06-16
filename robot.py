acc84e9a5d311449a2f749a5bba29d6d15240424
Arthur Antunes <arthuragds@gmail.com>
2018-06-16 19:59:48 -0300

    Adding functions that gets and sends information to the database multiple times.

--- robot.py
+++ robot.py
@@ -22,11 +22,23 @@ class RobotOfRequisitions(object):
 
         final_time = time.time()
 
-        total_time = initial_time - final_time
+        total_time = final_time - initial_time
 
-        print 'Response Time - GET request: {0}'.format(total_time)
+        #print 'Response Time - GET request: {0}'.format(total_time)
 
         conn.close()
+
+    def getContentFromTheDatabaseMultipleTimes(self, times):
+        initial_time = time.time()
+
+        for i in range(0, times):
+            self.getContentFromTheDatabase()
+
+        final_time = time.time()
+
+        total_time = final_time - initial_time
+
+        print 'Response Time - All GET requests: {0}'.format(total_time)
 
     # Sends a task to the dabase.
     def sendContentToTheDatabase(self):
@@ -41,11 +53,23 @@ class RobotOfRequisitions(object):
         
         final_time = time.time()
 
-        total_time = initial_time - final_time
+        total_time = final_time - initial_time
 
         print 'Response Time - POST request: {0}'.format(total_time)
 
         conn.close()
+
+    def sendContentToTheDatabaseMultipleTimes(self, times):
+        initial_time = time.time()
+
+        for i in range(0, times):
+            self.sendContentToTheDatabase()
+
+        final_time = time.time()
+
+        total_time = final_time - initial_time
+
+        print 'Response Time - All POST requests: {0}'.format(total_time)        
 
     # Saves the data.
     def write_data(self):
@@ -53,5 +77,7 @@ class RobotOfRequisitions(object):
 
 if __name__ == '__main__':
     myRobot = RobotOfRequisitions()
-    myRobot.getContentFromTheDatabase()
-    myRobot.sendContentToTheDatabase()
+    #myRobot.getContentFromTheDatabase()
+    #myRobot.sendContentToTheDatabase()
+    myRobot.getContentFromTheDatabaseMultipleTimes(500)
+    myRobot.sendContentToTheDatabaseMultipleTimes(500)
