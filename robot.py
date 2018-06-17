7e1ba361e3314ae4077a72b742d53d1f93106a38
wendleypf <wendley.franca@ccc.ufcg.edu.br>
2018-06-16 23:33:56 -0300

    implementing robot writing

--- robot.py
+++ robot.py
@@ -1,16 +1,35 @@
 import os
 import time
 import csv
+import ast
 import httplib, urllib
 from json import dumps
 
+http_method = {
+    'POST': 0,
+    'GET': 1,
+    'PUT' :2,
+    'DELETE': 3
+}
+
 class RobotOfRequisitions(object):
-    def __init__(self):
+
+    def __init__(self, method, number_of_requests = 1, output = None):
         self.url_default = '127.0.0.1:5000'
+        self.number_of_requests = number_of_requests
+        self.method = method
+        self.output = output
 
     # Inits requests.
     def start(self):
-        pass
+        if self.method == 1 and self.number_of_requests == 1:
+            self.getContentFromTheDatabase()
+        elif self.method == 1 and self.number_of_requests > 1:
+            self.getContentFromTheDatabaseMultipleTimes(self.number_of_requests)
+        elif self.method == 0 and self.number_of_requests == 1:
+            self.sendContentToTheDatabase()
+        elif self.method == 0 and self.number_of_requests > 1:
+            self.sendContentToTheDatabaseMultipleTimes(self.number_of_requests)
 
     # Gets all data from the database.
     def getContentFromTheDatabase(self):
@@ -20,25 +39,28 @@ class RobotOfRequisitions(object):
 
         conn.request("GET", "/tasks")
 
-        final_time = time.time()
+        # value may be incorrect
+        total_time = time.time() - initial_time
 
-        total_time = final_time - initial_time
+        response = ast.literal_eval(conn.getresponse().read())
+        server_resp = response['server_resp']
+        bd_time = response['bd_time']
+        cpu_usage = response['cpu_usage']
 
-        #print 'Response Time - GET request: {0}'.format(total_time)
-
+        self.write_data('GET', total_time, server_resp, bd_time, cpu_usage)
         conn.close()
 
     def getContentFromTheDatabaseMultipleTimes(self, times):
         initial_time = time.time()
-
+        # time of each requisition
+        wait_time = 60 / times
         for i in range(0, times):
             self.getContentFromTheDatabase()
+            time.sleep(wait_time)
 
-        final_time = time.time()
+        total_time = time.time() - initial_time
 
-        total_time = final_time - initial_time
-
-        print 'Response Time - All GET requests: {0}'.format(total_time)
+        print ('Response Time - All GET requests: {0}'.format(total_time))
 
     # Sends a task to the dabase.
     def sendContentToTheDatabase(self):
@@ -46,38 +68,56 @@ class RobotOfRequisitions(object):
 
         initial_time = time.time()
 
-        params = {"title": "testing...", "description":"testing..."}
-        headers = {"Content-type": "application/json", "Accept": "text/plain"}
+        params = {
+            "title": "testing...",
+            "description":"testing..."
+        }
+        headers = {
+            "Content-type": "application/json",
+            "Accept": "text/plain"
+        }
 
         conn.request("POST", "/tasks", dumps(params), headers)
-        
-        final_time = time.time()
+        # value may be incorrect
+        total_time = time.time() - initial_time
 
-        total_time = final_time - initial_time
+        response = ast.literal_eval(conn.getresponse().read())
+        server_resp = response['server_resp']
+        bd_time = response['bd_time']
+        cpu_usage = response['cpu_usage']
 
-        print 'Response Time - POST request: {0}'.format(total_time)
+        self.write_data('POST', total_time, server_resp, bd_time, cpu_usage)
+
+        print ('Response Time - POST request: {0}'.format(total_time))
 
         conn.close()
 
     def sendContentToTheDatabaseMultipleTimes(self, times):
         initial_time = time.time()
-
+        # time of each requisition
+        wait_time = 60 / times
         for i in range(0, times):
             self.sendContentToTheDatabase()
+            time.sleep(wait_time)
 
-        final_time = time.time()
+        total_time = time.time() - initial_time
 
-        total_time = final_time - initial_time
-
-        print 'Response Time - All POST requests: {0}'.format(total_time)        
+        print ('Response Time - All POST requests: {0}'.format(total_time))
 
     # Saves the data.
-    def write_data(self):
-        pass
+    def write_data(self, command, total_time, server_response, bd_response, cpu_usage):
+        fd = open(self.output, 'a')
+        output = csv.writer(fd, delimiter=' ', lineterminator="\n")
+        output.writerow([command, total_time, server_response, bd_response, cpu_usage])
+        fd.close()
 
 if __name__ == '__main__':
-    myRobot = RobotOfRequisitions()
-    #myRobot.getContentFromTheDatabase()
-    #myRobot.sendContentToTheDatabase()
-    myRobot.getContentFromTheDatabaseMultipleTimes(500)
-    myRobot.sendContentToTheDatabaseMultipleTimes(500)
+    # you have to rename the files you need to save
+    myRobot = RobotOfRequisitions(http_method['GET'], output = 'data-set/teste.csv')
+    myRobot.start()
+    myRobot = RobotOfRequisitions(http_method['GET'], 10, output='data-set/teste2.csv')
+    myRobot.start()
+    myRobot = RobotOfRequisitions(http_method['POST'], output='data-set/teste.csv')
+    myRobot.start()
+    myRobot = RobotOfRequisitions(http_method['POST'], 10, output='data-set/teste2.csv')
+    myRobot.start()
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
+import psutil
+import time
 
 e = create_engine('sqlite:///tasks.db')
 
@@ -9,18 +11,43 @@ api = Api(app)
 
 class Task(Resource):
     def get(self):
+        server_resp_timei = time.time()
+
+        bd_timei = time.time()
         conn = e.connect()
         query = conn.execute('select * from tasks')
-        return {'tasks': [i for i in query.cursor.fetchall()]}
+        bd_timef = time.time() - bd_timei
+
+        cpu_usage = psutil.cpu_percent(interval=0.1)
+
+        server_resp_timef = time.time() - server_resp_timei
+        print (server_resp_timef)
+        return {
+            'tasks': [i for i in query.cursor.fetchall()],
+            'server_resp': server_resp_timef,
+            'bd_time': bd_timef,
+            'cpu_usage': cpu_usage
+        }
 
     def post(self):
-        conn = e.connect()
+        server_resp_timei = time.time()
 
+        bd_timei = time.time()
+        conn = e.connect()
         title = request.json['title']
         description = request.json['description']
-
         query = conn.execute("insert into tasks values ('{0}', '{1}')".format(title, description))
-        return {'status':'success'}
+        bd_timef = time.time() - bd_timei
+
+        cpu_usage = psutil.cpu_percent(interval=0.1)
+        server_resp_timef = time.time() - server_resp_timei
+
+        return {
+            'status':'success',
+            'server_resp': server_resp_timef,
+            'bd_time': bd_timef,
+            'cpu_usage': cpu_usage
+        }
 
 #class TaskObject(Resource):
 #    def get(self, row_id):
