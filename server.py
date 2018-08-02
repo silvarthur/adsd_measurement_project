c0784ac0ce9a3fa6e4cf050805411170e66968e9
wendleypf <wendley.franca@ccc.ufcg.edu.br>
2018-08-01 22:22:34 -0300

    enviando metricas

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
+from datetime import datetime
 import psutil
 import time
+import csv
+import os
 
 e = create_engine('sqlite:///tasks.db')
 
@@ -11,25 +14,39 @@ api = Api(app)
 
 class Task(Resource):
     def get(self):
-        server_resp_timei = time.time()
+        log = request.args.get('log')
 
+        server_resp_timei = time.time()
         bd_timei = time.time()
         conn = e.connect()
         query = conn.execute('select * from tasks')
         bd_timef = time.time() - bd_timei
+        tasks = [i for i in query.cursor.fetchall()]
+
+        if log == 'true':
+            fd = open('log-get.csv', 'a')
+            output = csv.writer(fd, delimiter=' ', lineterminator="\n")
+            output.writerow([tasks, datetime.now()])
+            fd.close()
 
         cpu_usage = psutil.cpu_percent(interval=0.1)
 
+        pid = os.getpid()
+        py = psutil.Process(pid)
+        memory_usage = py.memory_info()[0] / 2. ** 30  # memoria usada em GB
+
         server_resp_timef = time.time() - server_resp_timei
-        print (server_resp_timef)
+
         return {
-            'tasks': [i for i in query.cursor.fetchall()],
+            'tasks': tasks,
             'server_resp': server_resp_timef,
             'bd_time': bd_timef,
-            'cpu_usage': cpu_usage
+            'cpu_usage': cpu_usage,
+            'memory_usage': memory_usage
         }
 
     def post(self):
+        log = request.args.get('log')
         server_resp_timei = time.time()
 
         bd_timei = time.time()
@@ -39,24 +56,30 @@ class Task(Resource):
         query = conn.execute("insert into tasks values ('{0}', '{1}')".format(title, description))
         bd_timef = time.time() - bd_timei
 
+        if log == 'true':
+            fd = open('log-post.csv', 'a')
+            output = csv.writer(fd, delimiter=' ', lineterminator="\n")
+            output.writerow([(title, description), datetime.now()])
+            fd.close()
+
         cpu_usage = psutil.cpu_percent(interval=0.1)
+
+        pid = os.getpid()
+        py = psutil.Process(pid)
+        memory_usage = py.memory_info()[0] / 2. ** 30  # memoria usada em GB
+
         server_resp_timef = time.time() - server_resp_timei
 
         return {
+            'task': {'title': title, 'description': description},
             'status':'success',
             'server_resp': server_resp_timef,
             'bd_time': bd_timef,
-            'cpu_usage': cpu_usage
+            'cpu_usage': cpu_usage,
+            'memory_usage': memory_usage
         }
 
-#class TaskObject(Resource):
-#    def get(self, row_id):
-#        conn = e.connect()
-#        query = conn.execute('SELECT * FROM tasks WHERE rowid={0}'.format(row_id))
-#        return {'tasks': [i for i in query.cursor.fetchall()]}
 
 api.add_resource(Task, '/tasks')
-#api.add_resource(TaskObject, '/task/<string:row_id>')
-
 if __name__ == '__main__':
     app.run()
