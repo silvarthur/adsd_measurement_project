a36d1c109f1e8c161f31db814b5cccf52e4a65ba
Hélisson Luiz <helisson.nascimento@ccc.ufcg.edu.br>
2018-08-05 02:45:24 -0300

    corrigindo o problema dos dados repetidos

--- metrics.py
+++ metrics.py
@@ -14,6 +14,7 @@ def plot(label, factor, title, key_data, data1, data2):
     plt.plot(x, y2, label='Log Inativo')
     plt.legend()
     plt.savefig(label + factor + '.pdf')
+    plt.gcf().clear()
     """ plt.show() """
 
 def plot_dois_mil(label, factor, title, key_data, data1, data2):
@@ -27,6 +28,7 @@ def plot_dois_mil(label, factor, title, key_data, data1, data2):
     plt.plot(x, y2, label='Log Inativo')
     plt.legend()
     plt.savefig(label + factor + '.pdf')
+    plt.gcf().clear()
     """ plt.show() """
 
 data_get_1 = p.read_csv('100-ativo/metricas-get.csv', encoding='utf-8')
@@ -45,7 +47,6 @@ plot_dois_mil('server_resp_timef', '_get2000','Tempo de Reposta do Servidor','se
 plot_dois_mil('bd_timef', '_get2000'         ,'Tempo de uso do Banco de Dados','bd_timef', data_get_3,data_get_4)
 plot_dois_mil('cpu_usage', '_get2000'        ,'Uso de CPU','cpu_usage', data_get_3,data_get_4)
 plot_dois_mil('memory_usage', '_get2000'     ,'Uso de Memoria','memory_usage', data_get_3,data_get_4)
-
 
 data_post_1 = p.read_csv('100-ativo/metricas-post.csv', encoding='utf-8')
 data_post_2 = p.read_csv('100-inativo/metricas-post.csv', encoding='utf-8')
